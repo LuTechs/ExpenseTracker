43c22376e092dea93449889d9e83aa00b7e57578
LENG UNG <ungleng@gmail.com>
2016-06-02 16:37:06 +1000

    update first test

--- ExpenseTracker.Tests/Controllers/Api/ExpenseApiControllerTest.cs
+++ ExpenseTracker.Tests/Controllers/Api/ExpenseApiControllerTest.cs
@@ -8,7 +8,7 @@ namespace ExpenseTracker.Tests.Controllers.Api
     public class ExpenseApiControllerTest
     {
 
-        [Fact (DisplayName = "Requst GetExpenses")]
+        [Fact (DisplayName = "Given page and pageSize GetExpensesWithPaginationResult should return expected results")]
         public void GetExpenseWithPaginationResult_WithPageAndPageSize_ReturnExpectedResult()
         {
             var mock=new Mock<IExpenseRepository>();
