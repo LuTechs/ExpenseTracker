3ad341e9b3c9437f4cd2b1954c7c1a3c969d886a
LENG UNG <ungleng@gmail.com>
2016-06-06 13:11:49 +1000

    improve angular ctrl

--- ExpenseTracker/App_Start/NinjectWebCommon.cs
+++ ExpenseTracker/App_Start/NinjectWebCommon.cs
@@ -1,19 +1,17 @@
+using System;
+using System.Web;
+using ExpenseTracker;
 using ExpenseTracker.Core.Repositories;
 using ExpenseTracker.Models;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(ExpenseTracker.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(ExpenseTracker.App_Start.NinjectWebCommon), "Stop")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
 
-namespace ExpenseTracker.App_Start
+namespace ExpenseTracker
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-
     public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
@@ -64,8 +62,8 @@ namespace ExpenseTracker.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Bind<ApplicationDbContext>().To<ApplicationDbContext>();
-            kernel.Bind<IExpenseRepository>().To<ExpenseRepository>();
+            kernel.Bind<ApplicationDbContext>().To<ApplicationDbContext>().InRequestScope();
+            kernel.Bind<IExpenseRepository>().To<ExpenseRepository>().InRequestScope();
         }        
     }
 }
--- ExpenseTracker/ClientScripts/controllers/ModalAddCtrl.js
+++ ExpenseTracker/ClientScripts/controllers/ModalAddCtrl.js
@@ -1,22 +1,25 @@
-﻿var modalController = angular.module('modalModule', []);
+﻿(function() {
+    var modalController = angular.module('modalModule', []);
 
-modalController.controller('ModalAddCtrl', [
-    '$scope', '$uibModalInstance', function ($scope,  $uibModalInstance) {
-        $scope.title = "Add new item";
-        $scope.buttonTitle = "Add New Item";
-        $scope.formData = {};
-        $scope.formData.date = moment().format("DD-MMMM-YYYY");
+    modalController.controller('ModalAddCtrl',
+    [
+        '$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
+            $scope.title = "Add new item";
+            $scope.buttonTitle = "Add New Item";
+            $scope.formData = {};
+            $scope.formData.date = moment().format("DD-MMMM-YYYY");
 
-        $scope.formData.categoryId = 19;
-        $scope.cancel = function () {
-             $uibModalInstance.dismiss('cancel');
-        };
-        $scope.save = function () {
-            $scope.formData.date = $scope.formData.date.toISOString();
-             $uibModalInstance.close($scope.formData);
+            $scope.formData.categoryId = 19;
+            $scope.cancel = function() {
+                $uibModalInstance.dismiss('cancel');
+            };
+            $scope.save = function() {
+                $scope.formData.date = $scope.formData.date.toISOString();
+                $uibModalInstance.close($scope.formData);
+            }
+            $scope.open = function() {
+                $scope.opened = true;
+            };
         }
-        $scope.open = function () {
-            $scope.opened = true;
-        };
-    }
-]);
+    ]);
+})();
